545248391e58a736e5208937a2c848de462d59f1
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-09-22 11:12:59 +0100

    Add project files.

---
+++ Snap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Snap
+{
+    class Program
+    {
+        public static bool Snap(string p1, string p2, string p3)
+        {
+            if ((p1.Equals(p2)) & (p1.Equals(p3)))
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+    }
+}
